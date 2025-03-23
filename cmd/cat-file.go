1f0245f3aae77a8f59824d7b6eff0cce4b225d6e
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2025-03-23 01:54:27 -0400

    draft: cat-file and hash-object files

--- cmd/cat-file.go
+++ cmd/cat-file.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+
+	"github.com/Jcho114/go-git/obj"
+	"github.com/Jcho114/go-git/repo"
 	"github.com/spf13/cobra"
 )
 
@@ -12,10 +16,41 @@ var catFileCmd = &cobra.Command{
 	Use:   "cat-file",
 	Short: "a very attempt at printing a git object to stdout",
 	Long:  "a very very bad attempt at printing a git object to stdout from scratch",
-	Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+	Args:  validateCatFileCmdArgs,
 	RunE:  runCatFile,
 }
 
+func validateCatFileCmdArgs(cmd *cobra.Command, args []string) error {
+	if err := cobra.ExactArgs(2)(cmd, args); err != nil {
+		return err
+	}
+
+	if err := cobra.OnlyValidArgs(cmd, args); err != nil {
+		return err
+	}
+
+	format := args[0]
+	if format != "blob" && format != "commit" && format != "tag" && format != "tree" {
+		return fmt.Errorf("invalid object type")
+	}
+
+	return nil
+}
+
 func runCatFile(cmd *cobra.Command, args []string) error {
+	objtype, objname := args[0], args[1]
+
+	repository, err := repo.FindRepository(".", true)
+	if err != nil {
+		return err
+	}
+
+	objname = obj.ObjectFind(repository, objname, objtype, true)
+	object, err := obj.ObjectRead(repository, objname)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(object.Serialize(repository))
 	return nil
 }
--- cmd/hash-object.go
+++ cmd/hash-object.go
@@ -1,10 +1,22 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/Jcho114/go-git/obj"
+	"github.com/Jcho114/go-git/repo"
 	"github.com/spf13/cobra"
 )
 
+var (
+	format string
+	write  bool
+)
+
 func init() {
+	hashObjectCmd.Flags().StringVar(&format, "t", "blob", "git object type")
+	hashObjectCmd.Flags().BoolVar(&write, "w", false, "actually write object to database")
 	rootCmd.AddCommand(hashObjectCmd)
 }
 
@@ -12,10 +24,50 @@ var hashObjectCmd = &cobra.Command{
 	Use:   "hash-object",
 	Short: "a very attempt at converting a file into a git object",
 	Long:  "a very very bad attempt at converting a file into a git object from scratch",
-	Args:  cobra.MatchAll(cobra.MinimumNArgs(1), cobra.OnlyValidArgs),
+	Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	RunE:  runHashObject,
 }
 
 func runHashObject(cmd *cobra.Command, args []string) error {
+	if format != "blob" && format != "commit" && format != "tag" && format != "tree" {
+		return fmt.Errorf("invalid object type")
+	}
+
+	var repository *repo.Repository
+	var err error
+	if write {
+		repository, err = repo.FindRepository(".", true)
+		if err != nil {
+			return err
+		}
+	} else {
+		repository = nil
+	}
+
+	path := args[0]
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil
+	}
+
+	var object obj.Object
+	switch format {
+	case "commit":
+		object = obj.NewCommit(data)
+	case "tree":
+		object = obj.NewTree(data)
+	case "tag":
+		object = obj.NewTag(data)
+	case "blob":
+		object = obj.NewBlob(data)
+	}
+
+	sha, err := obj.ObjectWrite(repository, object)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(sha)
+
 	return nil
 }
